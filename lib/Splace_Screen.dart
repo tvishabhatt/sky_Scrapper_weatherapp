de1fefa2e123b96c89f43ec4457b449b65282b97
Kamlesh <tvishabhatt1909@gmail.com>
2023-12-05 14:25:45 +0530

    "sky_weatherapp"

--- lib/Splace_Screen.dart
+++ lib/Splace_Screen.dart
@@ -11,13 +11,41 @@ class SplaceScreen extends StatefulWidget{
   }
 
 }
-class SplaceScreenState extends State<SplaceScreen>{
+class SplaceScreenState extends State<SplaceScreen> with TickerProviderStateMixin{
+
+  late AnimationController _animationController;
+  late Animation<double> _rotateAnimation;
+  late Animation<double> _scaleAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+
+    _rotateAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _animationController.forward();
+
+
     Future.delayed(Duration(seconds: 4),() =>
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage(),)),);
+        Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => HomePage(),)),);
   }
   @override
   Widget build(BuildContext context) {
@@ -28,17 +56,30 @@ class SplaceScreenState extends State<SplaceScreen>{
       Color(0xff293b47):
       Color(0xff3081cd),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image(image: AssetImage('assets/weather-splace-Screen.png'),height: 200,width: 200,),
+        child: RotationTransition(
+          turns: _rotateAnimation,
+
+
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image(image: AssetImage('assets/weather-splace-Screen.png'),height: 200,width: 200,),
+                ),
+                Text("Weathe app ",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),)
+              ],
             ),
-            Text("Weathe app ",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),)
-          ],
+          ),
         ),
       ),
     );
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
 }
